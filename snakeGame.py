5d7cbe683b04c35b7ff9da7e50a08e3a6e92ba8b
alwinruby <alwinsolanky@gmail.com>
2017-04-14 19:04:24 +0100

    events loop

--- snakeGame.py
+++ snakeGame.py
@@ -48,5 +48,21 @@ def gameOver():
     pygame.quit()#pygame exit
     sys.exit()#console exit
     
-
-
+# Logic for the game.
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT or event.key == ord('d')
+                changeto = 'RIGHT'
+            if event.key == pygame.K_LEFT or event.key == ord('a')
+                changeto = 'LEFT'
+            if event.key == pygame.K_UP or event.key == ord('w')
+                changeto = 'UP'
+            if event.key == pygame.K_DOWN or event.key == ord('s')
+                changeto = 'DOWN'
+            if event.key == pygame.K_ESCAPE:
+                pygame.event.post(pygame.event.Event(QUIT))
+            
