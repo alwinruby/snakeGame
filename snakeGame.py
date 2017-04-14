5536aa720b6c75cce69b803631eb6c4c2d2ec8f6
alwinruby <alwinsolanky@gmail.com>
2017-04-14 13:08:15 +0100

    initalised the game

--- snakeGame.py
+++ snakeGame.py
@@ -1,6 +1,13 @@
 # Snake Game!
+# by root
 
-import pygame
-import sys
-import random
-import time
+# game imports
+import pygame, sys, random, time
+
+check_errors = pygame.init()
+if check_errors[1] > 0:
+    print("(!) Had {0} initalising errors, exiting...". format(check_error[1]))
+    sys.exit(-1)
+else:
+    print("(+) PyGame succesfully initialised!")
+
